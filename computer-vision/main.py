d8859ea86576ef2fba0c41fed204bcb126d561ad
JZoares <joao.zanguinetti@hotmail.com>
2024-01-19 08:25:39 -0600

    Added Kubernetes configurations

--- computer-vision/main.py
+++ computer-vision/main.py
@@ -4,10 +4,10 @@ from dotenv import load_dotenv
 from proto.grpc_server import GrpcServer
 
 load_dotenv()
-COMPUTER_VISION_CONTAINER_HOST = os.environ.get("COMPUTER_VISION_CONTAINER_HOST") or "0.0.0.0"
-TRITON_GRPC_PORT = os.environ.get("TRITON_GRPC_PORT") or "8001"
-COMPUTER_VISION_PORT = os.environ.get("COMPUTER_VISION_PORT") or "8080"
-TRITON_SERVICE = os.environ.get("TRITON_SERVICE") or "triton-server"
+COMPUTER_VISION_CONTAINER_HOST = os.environ.get("COMPUTER_VISION_CONTAINER_HOST")
+TRITON_GRPC_PORT = os.environ.get("TRITON_GRPC_PORT")
+COMPUTER_VISION_PORT = os.environ.get("COMPUTER_VISION_PORT")
+TRITON_SERVICE = os.environ.get("TRITON_SERVICE")
 
 triton_client = TritonClient(TRITON_SERVICE, TRITON_GRPC_PORT)
 server = GrpcServer(triton_client)
